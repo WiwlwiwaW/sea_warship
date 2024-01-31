edfc4a2a73a0a255cb957f5b79b47113e1609f14
Илья <ilia.gorn123456789@mail.ru>
2024-02-01 00:32:14 +0300

    State

--- lib/blocs/game_state.dart
+++ lib/blocs/game_state.dart
@@ -4,3 +4,33 @@ part of 'game_bloc.dart';
 sealed class GameState {}
 
 final class GameInitial extends GameState {}
+
+final class ExpectationOpponentState extends GameState {
+  
+  ExpectationOpponentState();
+}
+
+final class CreateFieldState extends GameState {
+  
+  CreateFieldState();
+}
+
+final class ShotState extends GameState {
+  
+  ShotState();
+}
+
+final class SeeCreateFieldState extends GameState {
+  
+  SeeCreateFieldState();
+}
+
+final class SeeGameState extends GameState {
+  
+  SeeGameState();
+}
+
+final class SeeResultGameState extends GameState {
+  
+  SeeResultGameState();
+}
--- lib/blocs/table_state.dart
+++ lib/blocs/table_state.dart
@@ -4,3 +4,38 @@ part of 'table_bloc.dart';
 sealed class TableState {}
 
 final class TableInitial extends TableState {}
+
+final class EmptyTableState extends TableState {
+  
+  EmptyTableState();
+}
+
+final class PlacementShipState extends TableState {
+  
+  PlacementShipState();
+}
+
+final class GameMeState extends TableState {
+  
+  GameMeState();
+}
+
+final class GameYouState extends TableState {
+  
+  GameYouState();
+}
+
+final class SeePlacementState extends TableState {
+  
+  SeePlacementState();
+}
+
+final class SeeGameState extends TableState {
+  
+  SeeGameState();
+}
+
+final class SeeResultGameState extends TableState {
+  
+  SeeResultGameState();
+}
